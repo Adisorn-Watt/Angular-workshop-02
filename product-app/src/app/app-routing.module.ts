1c9cd47f1ce8e1deb2b91d6c3743b0fed7f6387a
Adisorn Wattana <adisorn.wattana@allianz.com>
2020-07-21 18:08:27 +0700

    Create routing, router link, class and data, fix only 2 decimal point

--- product-app/src/app/app-routing.module.ts
+++ product-app/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'list', component: ProductListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
--- product-app/src/app/product-list/product-list.component.ts
+++ product-app/src/app/product-list/product-list.component.ts
@@ -1,15 +1,23 @@
+import { Product } from './../models/product'
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
+
+  productList: Product[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    const p1 = new Product('y8.com', '01', 'Name001', 112.00, false, 4.2);
+    const p2 = new Product('y9.com', '02', 'Name002', 1150.50, true, 3.2);
+    const p3 = new Product('y10.com', '03', 'Name003', 144.42, false, 4.8);
+    this.productList.push(p1, p2, p3);
   }
 
 }
